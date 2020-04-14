971d11ad983a82320dab94a7cadc2be76e05f4a9
Geet <rgeet13@gmail.com>
2020-04-14 13:47:58 +0530

    Improved Accessibility

--- Whack-a-mole/app.js
+++ Whack-a-mole/app.js
@@ -1,4 +1,4 @@
-const square = document.querySelectorAll(".square");
+const squares = document.querySelectorAll(".square");
 const mole = document.querySelectorAll(".mole");
 const timeLeft = document.querySelector("#time-left");
 let score = document.querySelector("#score");
@@ -7,17 +7,17 @@ let result = 0;
 let currentTime = timeLeft.textContent;
 
 function randomSquare() {
-  square.forEach(className => {
+  squares.forEach(className => {
     className.classList.remove("mole");
   });
-  let randomPosition = square[Math.floor(Math.random() * 9)];
+  let randomPosition = squares[Math.floor(Math.random() * 9)];
   randomPosition.classList.add("mole");
 
   // assign the id of the randomPosition to hitPosition for us to use later
   hitPosition = randomPosition.id;
 }
 
-square.forEach(id => {
+squares.forEach(id => {
   id.addEventListener("mouseup", () => {
     if (id.id === hitPosition) {
       result = result + 1;
@@ -28,7 +28,7 @@ square.forEach(id => {
 
 function moveMole() {
   let timerId = null;
-  timerId = setInterval(randomSquare, 1000);
+  timerId = setInterval(randomSquare, 800);
 }
 
 moveMole();
@@ -38,8 +38,8 @@ function countDown() {
   timeLeft.textContent = currentTime;
 
   if (currentTime === 0) {
-    clearInterval(timerId);
     alert("Game Over! Your Final Score is" + result);
+    clearInterval(timerId);
   }
 }
 
